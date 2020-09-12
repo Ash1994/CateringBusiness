fef1404c5f9a67fb6e38a87bef498245d49f560c
Ashwin Mahatkar <52129131+Ash1994@users.noreply.github.com>
2020-09-12 11:35:57 +0530

    Commit Two

--- CateringBusiness/CateringBusinessLogin/src/main/java/com/catering/in/CateringBusinessLogin/user/UserController.java
+++ CateringBusiness/CateringBusinessLogin/src/main/java/com/catering/in/CateringBusinessLogin/user/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
-//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @Slf4j
 public class UserController {
@@ -17,14 +16,14 @@ public class UserController {
         this.svc = svc;
     }
 
-    @GetMapping("/api/user/getById/{id}")
+    @GetMapping("/user/getById/{id}")
     private ResponseEntity<User> getUserById(@PathVariable Integer id) {
         User user = svc.getUserById(id);
         log.info("user:: {}", user);
         return ResponseEntity.ok(user);
     }
 
-    @PostMapping("/api/user")
+    @PostMapping("/user")
     private String getUserById(@RequestBody User user) {
         svc.saveAll(user);
         return "Created";
